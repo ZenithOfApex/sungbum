e1b333266e2d8af6467205b69c8c73cb8ed78a5a
KangSungBum <peter9706@naver.com>
2021-09-08 16:14:17 +0900

    Problem Solved: file name refactored

--- src/BaekJoon/wordCount_1152.java
+++ src/BaekJoon/wordCount_1152.java
@@ -3,7 +3,7 @@ package BaekJoon;
 import java.util.Scanner;
 import java.util.StringTokenizer;
 
-public class Q1152 {
+public class wordCount_1152 {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
 
--- src/StackQueue/test2.java
+++
@@ -1,7 +0,0 @@
-package StackQueue;
-
-public class test2 {
-    public static void main(String[] args){
-        System.out.println("test code2");
-    }
-}
