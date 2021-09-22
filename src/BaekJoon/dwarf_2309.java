e661b446d77dfd8af33eeb551f60ee9c43587a56
KangSungBum <peter9706@naver.com>
2021-09-22 18:56:40 +0900

    Problem Solved but not accepted

--- src/BaekJoon/dwarf_2309.java
+++ src/BaekJoon/dwarf_2309.java
@@ -1,40 +1,32 @@
 package BaekJoon;
 
 import java.util.ArrayList;
-import java.util.Arrays;
+import java.util.Collections;
 import java.util.Scanner;
 
 public class dwarf_2309 {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         int[] arr = new int[9];
+        ArrayList<Integer> dwarf = new ArrayList<>();
+        int sum=0;
         for(int i=0;i<9;i++){
-            arr[i] = sc.nextInt();
+            dwarf.add(sc.nextInt());
         }
-        sc.close();
-
-        Arrays.sort(arr);
-        for(int i=0;i<arr.length;i++){
-            System.out.println(arr[i]);//오름차순 정렬
+        for (int i = 0; i<dwarf.size(); i++) {
+            sum += dwarf.get(i);
         }
-        System.out.println();
-        System.out.println();
-        ArrayList<Integer> answer = new ArrayList<>();
 
-        Combination.combination comb = new Combination.combination(arr.length, 7);
-        comb.combination(arr, 0, 0, 0);
-        ArrayList<ArrayList<Integer>> result = comb.getResult();
-        for(int i=0;i<result.size();i++){
-            int sum=0;
-            for(int j=0;j<result.get(i).size();j++){
-                sum+=result.get(i).get(j);
-            }
-            if(sum==100){
-                for(int j=0;j<result.get(i).size();j++){
-                    answer = result.get(i);
-//                    System.out.println(result.get(i).get(j));
-                }
-                System.exit(0);
+        for(int i=0;i< dwarf.size();i++){
+            for (int j = i + 1; j < dwarf.size(); j++) {
+                if (sum - dwarf.get(i) - dwarf.get(j) == 100) {
+                    dwarf.remove(i);
+                    dwarf.remove(j);
+                    Collections.sort(dwarf);
+                    for (int k = 0; k < dwarf.size(); k++) {
+                        System.out.println(dwarf.get(k));
+                    }
+                }else continue;
             }
         }
     }
