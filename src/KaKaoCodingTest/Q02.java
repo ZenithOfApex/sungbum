4cb8c0ac9826c9107d7fb014b32a8b13a5aa0cda
KangSungBum <peter9706@naver.com>
2021-09-22 14:41:01 +0900

    Problem Solved: Initial Code

--- src/KaKaoCodingTest/Q02.java
+++ src/KaKaoCodingTest/Q02.java
@@ -1,42 +1,63 @@
 package KaKaoCodingTest;
 
+import java.util.ArrayList;
+
 public class Q02 {
     public static void main(String[] args) {
-        int n=437674;
-        int k=3;
+        int n=110011;
+        int k=10;
+//        int n = 437674;
+//        int k=3;
         int result = solution(n, k);
         System.out.println(result);
     }
     public static int solution(int n, int k) {
         int answer = -1;
-
-        String[] result = {};
-        result = String.valueOf(conversion(n, k)).split("0");
-        int count=0;
+        String str = conversion(n, k);
+        System.out.println(str);
+        Long strNum = Long.parseLong(str);
+        System.out.println("strNum = " + strNum);
+        String[] result = str.split("0");
+        ArrayList<String> result2 = new ArrayList<String>();
+//        String[] result2 = new String[result.length * 2];
         for(int i=0;i<result.length;i++){
-            for(int j=2;j<n;j++){
-                if(Integer.parseInt(result[i])%j ==0)
-                    continue;
-                else if(j==n-1)
-                    count++;
+            System.out.println("result = " + result[i]);
+            if (Integer.parseInt(result[i]) >= 2) {
+                result2.add(result[i]);
+            }
+        }
+        for(int i=0;i<result2.size();i++){
+            System.out.println("result2.get(i) = " + result2.get(i));
+        }
+        int count=0;
+        for(int i=0;i<result2.size();i++){
+            System.out.println(Integer.parseInt(result2.get(i))+" a");
+            int checkNum = Integer.parseInt(result2.get(i));
+            if(checkNum==1) break;
+            if(checkNum==2){
+                count++;
+            }else{
+                boolean prime = false;
+                for(int j=2;j<checkNum;j++){
+                    if(checkNum%j==0){
+                        prime = true;
+                        System.out.println(checkNum);
+                        break;
+                    }
+                }
+                if(prime==false) count++;
             }
         }
         answer = count;
         return answer;
     }
 
-    public static StringBuilder conversion(int number, int n){
+    public static String conversion(int number, int n){
         StringBuilder sb = new StringBuilder();
-        int now = number;
-        while(now>0){
-            if(now%n<10){
-                sb.append(now % n);
-            }
-            else{
-                sb.append((char) (now % n - 10 + 'A'));
-            }
-            now /=n;
+        while (number >= 1) {
+            sb.insert(0, number % n);
+            number/=n;
         }
-        return sb.reverse();
+        return sb.toString();
     }
 }
