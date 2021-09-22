3d0f939b30f1b6525109d64f554a5f88e3bc15b3
KangSungBum <peter9706@naver.com>
2021-09-22 18:52:00 +0900

    Problem Solved

--- src/BaekJoon/sleep_1652.java
+++ src/BaekJoon/sleep_1652.java
@@ -7,22 +7,53 @@ public class sleep_1652 {
         Scanner sc = new Scanner(System.in);
         int n = sc.nextInt();
         sc.nextLine();
-        StringBuilder sb = new StringBuilder();//세로로 저장하기 위함
-        String[][] str = new String[n][n];
-        int garo, saero;
-        for(int i=0;i<n;i++){
-            int temp=0;
-            String s = sc.nextLine();
-            String[] arr = s.split("X");
-            for(int j=0;j<arr.length;j++){
-                if (arr[i].contains(".") && arr[i].length() >= 2) {
-                    temp++;
-                }
+        String[][] map = new String[n][n];
+        String[][] transposedMap = new String[n][n];
+        int garo=0;
+        int saero=0;
+
+        for (int i = 0; i < n; i++) {
+            String str= sc.nextLine();
+            for (int j = 0; j < n; j++) {
+                map[i][j] = String.valueOf(str.charAt(j));
             }
-            System.out.println(temp);
-            System.out.println(arr[i].length());
-            temp=0;
+        }
+        sc.close();
+        garo = checkPlace(map);
+        transposedMap = transpose(map);
+        saero = checkPlace(transposedMap);
+
+        System.out.printf("%d %d", garo, saero);
+    }
+
+    public static int checkPlace(String[][] map) {
+        int cnt =0;
+        StringBuilder sb = new StringBuilder();
+
+        for (int i = 0; i < map.length; i++) {
+            for (int j = 0; j < map.length; j++) {
+                sb.append(map[i][j]);
+            }
+            String[] temp = String.valueOf(sb).split("X");
+            for (int j = 0; j < temp.length; j++) {
+                if (temp[j].contains(".") && temp[j].length() >= 2) {
+                    cnt++;
+                }else continue;
+            }
+            sb.setLength(0);
         }
 
+
+        return cnt;
+    }
+
+    public static String[][] transpose(String[][] map){
+        String[][] transposed = new String[map.length][map.length];
+        for(int i=0;i<map.length;i++){
+            for (int j = 0; j < map.length; j++) {
+                transposed[i][j] = map[j][i];
+            }
+        }
+        return transposed;
     }
 }
