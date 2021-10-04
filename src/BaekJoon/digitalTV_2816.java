79bc32bffcfc8972600a6d92bfa49c750854846f
KangSungBum <peter9706@naver.com>
2021-10-04 17:59:53 +0900

    Problem Solved

--- src/BaekJoon/digitalTV_2816.java
+++ src/BaekJoon/digitalTV_2816.java
@@ -6,25 +6,55 @@ public class digitalTV_2816 {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         int num = sc.nextInt();
+        sc.nextLine();
         String[] channel = new String[num];
+
         for (int i = 0; i < num; i++) {
-            String temp = sc.nextLine();
-            channel[i] = temp;
+            channel[i] = sc.nextLine();
         }
+        sc.close();
+        StringBuilder sb = new StringBuilder();
+
+        int targetOneIndex=0;
+        int targetTwoIndex=0;
+
         for (int i = 0; i < channel.length; i++) {
-            System.out.println(channel[i]);
+            if (channel[i].equals("KBS1")) {
+                targetOneIndex = i;
+            } else if (channel[i].equals("KBS2")) {
+                targetTwoIndex = i;
+            }
         }
-
-        while (!checkFinished(channel)) {
-
+        int moveIndex =0;
+        if (targetOneIndex > targetTwoIndex) {
+            for (int i = 0; i < targetOneIndex; i++) {
+                sb.append("1");
+            }
+            for (int i = 0; i < targetOneIndex; i++) {
+                sb.append("4");
+            }
+            targetTwoIndex++;
+            for (int i = 0; i < targetTwoIndex; i++) {
+                sb.append("1");
+            }
+            for (int i = 0; i < targetTwoIndex - 1; i++) {
+                sb.append("4");
+            }
+        } else if (targetOneIndex < targetTwoIndex) {
+            for (int i = 0; i < targetOneIndex; i++) {
+                sb.append("1");
+            }
+            for (int i = 0; i < targetOneIndex; i++) {
+                sb.append("4");
+            }
+            for (int i = 0; i < targetTwoIndex; i++) {
+                sb.append("1");
+            }
+            for (int i = 0; i < targetTwoIndex-1; i++) {
+                sb.append("4");
+            }
         }
-    }
+        System.out.println(String.valueOf(sb));
 
-    public static boolean checkFinished(String[] arr){
-        boolean status =false;
-        if (arr[0].equals("KBS1") && arr[1].equals("KBS2")) {
-            status = true;
-        }
-        return status;
     }
 }
