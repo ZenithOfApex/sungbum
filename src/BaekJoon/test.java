fcb45d1cd4bf62a88f578468b3c30684f8893209
KangSungBum <peter9706@naver.com>
2021-10-06 16:45:14 +0900

    git test02

--- src/BaekJoon/test.java
+++ src/BaekJoon/test.java
@@ -3,5 +3,6 @@ package BaekJoon;
 public class test {
     public static void main(String[] args) {
         System.out.println("test number 1");
+        System.out.println("test number 2");
     }
 }
