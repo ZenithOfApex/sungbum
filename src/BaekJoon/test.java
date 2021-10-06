bd733f85b78e107419e90c55238ff0cac88f09d7
KangSungBum <peter9706@naver.com>
2021-10-06 14:16:37 +0900

    test code3

--- src/BaekJoon/test.java
+++ src/BaekJoon/test.java
@@ -4,5 +4,6 @@ public class test {
     public static void main(String[] args) {
         System.out.println("test code");
         System.out.println("test code2");
+        System.out.println("test code3");
     }
 }
