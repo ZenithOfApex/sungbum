9465cea46b3e5bfb3b17ccb3019c3d9b2a71400b
KangSungBum <peter9706@naver.com>
2021-10-06 14:29:06 +0900

    test code5

--- src/BaekJoon/test.java
+++ src/BaekJoon/test.java
@@ -6,5 +6,6 @@ public class test {
         System.out.println("test code2");
         System.out.println("test code3");
         System.out.println("test code4");
+        System.out.println("test code5");
     }
 }
