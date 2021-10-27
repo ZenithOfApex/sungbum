97388dc36b587bf8072af481b12cc31f6d1c63fc
KangSungBum <peter9706@naver.com>
2021-10-27 16:19:03 +0900

    Problem Solved

---
+++ src/BaekJoon/addCycle_1110.java
@@ -0,0 +1,31 @@
+package BaekJoon;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class addCycle_1110 {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine()," ");
+        int n = Integer.parseInt(st.nextToken());
+        int breakNum = n;
+        int cycleCnt =0;
+
+        while (true) {
+
+            int addNum = n;
+            int ten = addNum / 10;
+            int one = addNum % 10;
+            int sum = (ten + one) % 10;
+
+            n = ((n%10)*10)+sum;
+            cycleCnt++;
+            if (n == breakNum) {
+                break;
+            }
+        }
+        System.out.println(cycleCnt);
+    }
+}
