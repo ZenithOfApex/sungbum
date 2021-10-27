b898eb2372822ad5fe460b75c1cd1284f72a5fe3
KangSungBum <peter9706@naver.com>
2021-10-27 16:02:14 +0900

    Problem Solved

--- src/BaekJoon/noHearSee_1764.java
+++ src/BaekJoon/noHearSee_1764.java
@@ -1,37 +1,38 @@
 package BaekJoon;
 
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
 import java.util.ArrayList;
 import java.util.Collections;
-import java.util.Scanner;
+import java.util.HashSet;
+import java.util.StringTokenizer;
 
 public class noHearSee_1764 {
-    public static void main(String[] args) {
-        Scanner sc = new Scanner(System.in);
-        int n = sc.nextInt();
-        int m = sc.nextInt();
-        sc.nextLine();
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine()," ");
+        int n = Integer.parseInt(st.nextToken());
+        int m = Integer.parseInt(st.nextToken());
 
-        ArrayList<String> noSee = new ArrayList<>();
-        ArrayList<String> noHear = new ArrayList<>();
+        HashSet<String> hs = new HashSet<>();
+        String[] noSee = new String[m];
+        String[] noHear = new String[n];
         ArrayList<String> result = new ArrayList<>();
 
         for (int i = 0; i < n; i++) {
-            String temp_name = sc.nextLine();
-            noHear.add(temp_name);
-        }
-        for (int i = 0; i < m; i++) {
-            String temp_name = sc.nextLine();
-            noSee.add(temp_name);
+            String temp_name = br.readLine();
+            hs.add(temp_name);
         }
         int cnt =0;
-        for (int i = 0; i < noHear.size(); i++) {
-            for (int j = 0; j < noSee.size(); j++) {
-                if (noHear.get(i).equals(noSee.get(j))) {
-                    cnt++;
-                    result.add(noHear.get(i));
-                }
+        for (int i = 0; i < m; i++) {
+            String temp_name = br.readLine();
+            if (hs.contains(temp_name)) {
+                cnt++;
+                result.add(temp_name);
             }
         }
+
         Collections.sort(result);
         System.out.println(cnt);
         for (int i = 0; i < result.size(); i++) {
