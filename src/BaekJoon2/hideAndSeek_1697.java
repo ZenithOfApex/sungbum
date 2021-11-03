c5af3462b2dc0680e9b0f5d3277113d3d1d8f962
KangSungBum <peter9706@naver.com>
2021-11-03 12:22:37 +0900

    Code Refactored

--- src/BaekJoon2/hideAndSeek_1697.java
+++ src/BaekJoon2/hideAndSeek_1697.java
@@ -3,28 +3,80 @@ package BaekJoon2;
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
-import java.util.HashMap;
-import java.util.StringTokenizer;
+import java.util.*;
 
 public class hideAndSeek_1697 {
-    public static void main(String[] args) throws IOException {
-        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        StringTokenizer st = new StringTokenizer(br.readLine()," ");
-        int n = Integer.parseInt(st.nextToken());
-        int m = Integer.parseInt(st.nextToken());
+//    static int INF = 100;
+//    public static void main(String[] args) throws IOException {
+//        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+//        StringTokenizer st = new StringTokenizer(br.readLine()," ");
+//        int n = Integer.parseInt(st.nextToken());
+//        int k = Integer.parseInt(st.nextToken());
+//        int[] numbers = new int[100001];
+//        int level = 0;
+//
+//        for (int i = 0; i < numbers.length; i++) {
+//            numbers[i] = INF;
+//        }
+//        numbers[n] = level;
+//        while (true) {
+//            for (int i = 0; i < numbers.length; i++) {
+//                if (numbers[i] == level) {
+//                    if (i - 1 >= 0 && i + 1 < numbers.length && 2 * i < numbers.length) {
+//                        if (numbers[i - 1] == INF) {
+//                            numbers[i - 1] = level + 1;
+//                        }
+//                        if (numbers[i + 1] == INF) {
+//                            numbers[i + 1] = level + 1;
+//                        }
+//                        if (numbers[2 * i] == INF) {
+//                            numbers[i * 2] = level+1;
+//                        }
+//                    }
+//                }
+//            }
+//            if (numbers[k] != INF) {
+//                System.out.println(numbers[k]);
+//                break;
+//            }
+//            level++;
+//        }
+//    }
 
-        HashMap<Integer, Integer> hm = new HashMap<>();
+    public static void main(String[] args) throws IOException{
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine(), " ");
+        int n = Integer.parseInt(st.nextToken());
+        int k = Integer.parseInt(st.nextToken());
+        ArrayList<Integer> ar = new ArrayList<Integer>();
+        br.close();
+
         int level = 0;
-        while (true) {
-            if (hm.containsKey(m)) {
-                System.out.println(hm.get(m));
+        ar.add(n);
+        while(true){
+            System.out.println(ar);
+            if (ar.contains(k)) {
+                System.out.println(level);
                 break;
             }
-            hm.put(n, level);
-            hm.put(n+1, level+1);
-            hm.put(n - 1, level + 1);
-            hm.put(n * 2, level + 1);
+
+            for (int i = 0; i < ar.size(); i++) {
+                int number = ar.get(i);
+                int addNum = number+1;
+                int subNum = number-1;
+                int mulNum = number*2;
+                if (!ar.contains(addNum) && addNum >= 0) {
+                    ar.add(addNum);
+                }
+                if (!ar.contains(subNum) && subNum >= 0) {
+                    ar.add(subNum);
+                }
+                if (!ar.contains(mulNum) && mulNum >= 0) {
+                    ar.add(mulNum);
+                }
+            }
             level++;
         }
+        System.out.println();
     }
 }
