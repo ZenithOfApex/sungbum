bc06a057c7ae2a0c642b1fdf0811d0f20cf490ce
KangSungBum <peter9706@naver.com>
2021-11-10 16:43:30 +0900

    Problem Solved

---
+++ src/BaekJoon2/SnailGoesUp_2869.java
@@ -0,0 +1,21 @@
+package BaekJoon2;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class SnailGoesUp_2869 {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        int a = Integer.parseInt(st.nextToken());
+        int b = Integer.parseInt(st.nextToken());
+        int v = Integer.parseInt(st.nextToken());
+        br.close();
+
+
+        double turns = Math.ceil((double)(v-a)/(a-b))+1;
+        System.out.println((int)turns);
+    }
+}
