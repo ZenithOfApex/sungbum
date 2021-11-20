1454a01c4a68da15052368876130181a282926fe
KangSungBum <peter9706@naver.com>
2021-11-20 21:27:41 +0900

    Problem Solved

--- src/programmers/FailRatio.java
+++ src/programmers/FailRatio.java
@@ -2,6 +2,7 @@
 package programmers;
 
 import java.util.Arrays;
+import java.util.Comparator;
 
 public class FailRatio {
     public static void main(String[] args) {
@@ -13,9 +14,9 @@ public class FailRatio {
         }
     }
 
-    public static int[] solution(int n, int[] stages){//N+1이여서 0인 경우랑, 해당 스테이지에 머무르는 사람 없어서 0인 경우 구분 필요
-        int[] answer = {};
-        double[] result = new double[n];//stage-ratio 결과
+    public static int[] solution(int n, int[] stages){//N+1이여서 0인 경우랑, 해당 스테이지에 머무르는 사람 없어서 0인 경우 구분 필요 if there is bigger num in array
+        int[] answer = new int[n];
+        double[][] result = new double[n][2];//stage-ratio 결과
         int curCnt=stages.length;//ratio 분모
 
         for (int i = 1; i <= n; i++) {//stage별 측정
@@ -26,13 +27,33 @@ public class FailRatio {
                     cnt++;
                 }
             }
-            ratio = (double) i/curCnt;
-            result[i-1] = ratio;
+            ratio = (double) cnt/curCnt;
+            result[i-1][0]=i;
+            result[i-1][1] = ratio;
             curCnt -=cnt;
         }
+        //각 stage 별 실패율이 담기게 되는데 이를 순서에 맞게 정렬 - Collections.sort(result, Comparable<>)
 
-        Arrays.sort(result);
+        Arrays.sort(result, new Comparator<double[]>() {
+            @Override
+            public int compare(double[] o1, double[] o2) {
+                if (o1[1] == o2[1]) {//실패율이 같은 경우에는
+                    return Double.compare(o2[0], o1[0]);
+//                    return Double.compare(o1[0], o2[0]);
+                }
+                return Double.compare(o1[1],o2[1]);
+            }
+        });
 
+//        for (int i = 0; i < n; i++) {
+//            System.out.println(result[i][0]);
+//            System.out.println(result[i][1]);
+//            System.out.println();
+//        }
+
+        for (int i = 0; i < n; i++) {
+            answer[i] = (int)result[n-i-1][0];
+        }
 
         return answer;
     }
