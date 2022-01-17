6a76fa7fe508c09e1eeec2168f71f760430ba3ae
KangSungBum <peter9706@naver.com>
2022-01-17 11:08:27 +0900

    Example Code

---
+++ src/SDS/FiboEx.java
@@ -0,0 +1,13 @@
+package SDS;
+
+public class FiboEx {
+    public static void main(String[] args) {
+        System.out.println("fib(7) = " + fib(7));
+    }
+
+    public static int fib(int n) {
+        if (n == 1 || n == 2) {
+            return 1;
+        } else return fib(n - 1) + fib(n - 2);
+    }
+}
