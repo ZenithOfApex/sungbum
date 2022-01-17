f90a6c2c73c00d6942ef508e251934b316c7e311
KangSungBum <peter9706@naver.com>
2022-01-17 17:54:48 +0900

    Problem Solving

---
+++ src/SDS/HanSoo_1065.java
@@ -0,0 +1,22 @@
+package SDS;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class HanSoo_1065 {
+    private static int N;
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        N = Integer.parseInt(br.readLine());
+        System.out.println(checkHanSoo(N));;
+    }
+
+    public static int checkHanSoo(int n) {
+        for (int i = 1; i <= n; i++) {
+
+        }
+    }
+}
