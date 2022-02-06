d35dd63a0290373bbcb34b7c62fd57b94d84d157
KangSungBum <peter9706@naver.com>
2022-02-06 17:22:59 +0900

    Problem Solved:Time Overflow

--- src/BaekJoon3/TreeBusiness_16235.java
+++ src/BaekJoon3/TreeBusiness_16235.java
@@ -1,15 +1,13 @@
 package BaekJoon3;
 
-import java.io.BufferedReader;
-import java.io.IOException;
-import java.io.InputStreamReader;
+import java.io.*;
 import java.util.*;
 
 public class TreeBusiness_16235 {
 
     static int[][] nutrition;
     static int[][] injection;
-    static Queue<Tree> treeList = new LinkedList<>();
+    static Deque<Tree> treeList = new LinkedList<>();
     static Queue<Tree> deadTree = new LinkedList<>();
     static Queue<Tree> autumnTree = new LinkedList<>();
 
@@ -20,33 +18,29 @@ public class TreeBusiness_16235 {
 
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
         StringTokenizer st = new StringTokenizer(br.readLine(), " ");
 
         N = Integer.parseInt(st.nextToken());
         M = Integer.parseInt(st.nextToken());
         K = Integer.parseInt(st.nextToken());
 
-        nutrition = new int[N][N];
-        for (int i = 0; i < N; i++) {
-            for (int j = 0; j < N; j++) {
+        nutrition = new int[N + 1][N + 1];
+        injection = new int[N + 1][N + 1];
+        for (int i = 1; i <= N; i++) {
+            st = new StringTokenizer(br.readLine(), " ");
+            for (int j = 1; j <= N; j++) {
+                injection[i][j] = Integer.parseInt(st.nextToken());
                 nutrition[i][j] = 5;
             }
         }
 
-        injection = new int[N][N];
-        for (int i = 0; i < N; i++) {
-            st = new StringTokenizer(br.readLine(), " ");
-            for (int j = 0; j < N; j++) {
-                injection[i][j] = Integer.parseInt(st.nextToken());
-            }
-        }
-
-        for (int i = 0; i < M; i++) {
+        for (int i = 1; i <= M; i++) {
             st = new StringTokenizer(br.readLine(), " ");
             int x = Integer.parseInt(st.nextToken());
             int y = Integer.parseInt(st.nextToken());
             int age = Integer.parseInt(st.nextToken());
-            treeList.add(new Tree(x-1, y-1, age));
+            treeList.add(new Tree(x, y, age));
         }
 
         while (K-- > 0) {
@@ -56,64 +50,64 @@ public class TreeBusiness_16235 {
             winter();
         }
 
-        System.out.println(treeList.size());
-
+        bw.write(treeList.size() + "\n");
+        bw.flush();
+        bw.close();
         br.close();
     }
     //양분 흡수(어린나무부터), 흡수못한 나무는 즉사
     private static void spring() {
-        while (!treeList.isEmpty()) {
+        for (int i = 0; i < treeList.size();) {
             Tree polled = treeList.poll();
-            int polledX = polled.x;
-            int polledY = polled.y;
-            int polledAge = polled.age;
-
-            if (polledAge > nutrition[polledX][polledY]) {
-                deadTree.offer(polled);
+            if (nutrition[polled.x][polled.y] >= polled.age) {
+                nutrition[polled.x][polled.y] -= polled.age;
+                polled.age++;
+                i++;
+                treeList.add(polled);
             } else {
-                nutrition[polledX][polledY] -= polledAge;
-                autumnTree.offer(new Tree(polledX, polledY, polledAge + 1));
+                deadTree.add(polled);
             }
         }
     }
     //죽은 나무의 나이/2 값을 양분에 추가
     private static void summer() {
-        while (!deadTree.isEmpty()) {
-            Tree dead = deadTree.poll();
-
-            int deadNut = dead.age / 2;
-            nutrition[dead.x][dead.y] += deadNut;
-
+        for (Tree tree : deadTree) {
+            nutrition[tree.x][tree.y] += tree.age / 2;
         }
     }
 
     //나이가 5의 배수인 나무 8방에 새로운 묘목(1살) 추가
     private static void autumn() {
+
+        for (Tree tree : treeList) {
+            if (tree.age % 5 == 0) {
+                autumnTree.add(tree);
+            }
+        }
         while (!autumnTree.isEmpty()) {
-            Tree autumn = autumnTree.poll();
-            if (autumn.age % 5 == 0) {//5의 배수라면
-                for (int i = 0; i < 8; i++) {
-                    int nx = autumn.x + dx[i];
-                    int ny = autumn.y + dy[i];
-                    if (inRange(nx, ny)) {
-                        treeList.add(new Tree(nx, ny, 1));
-                    }
+            Tree t = autumnTree.poll();
+
+            for (int i = 0; i < 8; i++) {
+                int nx = t.x + dx[i];
+                int ny = t.y + dy[i];
+                if (inRange(nx, ny)) {
+                    treeList.addFirst(new Tree(nx, ny, 1));
                 }
             }
-            treeList.offer(autumn);
+
         }
     }
     //영양분 추가
     private static void winter() {
-        for (int i = 0; i < N; i++) {
-            for (int j = 0; j < N; j++) {
+        for (int i = 1; i <= N; i++) {
+            for (int j = 1; j <= N; j++) {
                 nutrition[i][j] += injection[i][j];
             }
         }
     }
 
     private static boolean inRange(int x, int y) {
-        return x >= 0 && x < N && y >= 0 && y < N;
+        return x >= 1 && x <= N && y >= 1 && y <= N;
     }
 
     static class Tree implements Comparable<Tree>{
@@ -122,6 +116,7 @@ public class TreeBusiness_16235 {
         int age;
 
         public Tree(int x, int y, int age) {
+            super();
             this.x = x;
             this.y = y;
             this.age = age;
@@ -129,7 +124,7 @@ public class TreeBusiness_16235 {
 
         @Override
         public int compareTo(Tree o) {
-            return age - o.age;
+            return this.age - o.age;
         }
     }
 }
