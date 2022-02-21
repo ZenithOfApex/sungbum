4fc7b30dd707619c9031f4bb7fa41250448acb54
KangSungBum <peter9706@naver.com>
2022-02-21 16:09:43 +0900

    Problem Answer

---
+++ src/BaekJoon3/HeightSequence2_2458.java
@@ -0,0 +1,72 @@
+package BaekJoon3;
+
+import java.io.*;
+import java.util.StringTokenizer;
+
+public class HeightSequence2_2458 {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+
+        int N = Integer.parseInt(st.nextToken());
+        int M = Integer.parseInt(st.nextToken());
+
+        boolean[][] arr = new boolean[N + 1][N + 1];
+        boolean[][] reverseArr = new boolean[N + 1][N + 1];
+
+        for (int i = 0; i < M; i++) {
+            st = new StringTokenizer(br.readLine());
+            int a = Integer.parseInt(st.nextToken());
+            int b = Integer.parseInt(st.nextToken());
+
+            arr[a][b] = true;
+            reverseArr[b][a] = true;
+        }
+
+        for (int k = 1; k <= N; k++) {
+            for (int i = 1; i <= N; i++) {
+                for (int j = 1; j <= N; j++) {
+                    if (arr[i][k] && arr[k][j]) {
+                        arr[i][j] = true;
+                    }
+                }
+            }
+        }
+
+        for (int k = 1; k <= N; k++) {
+            for (int i = 1; i <= N; i++) {
+                for (int j = 1; j <= N; j++) {
+                    if (reverseArr[i][k] && reverseArr[k][j]) {
+                        reverseArr[i][j] = true;
+                    }
+                }
+            }
+        }
+
+        for (int i = 1; i <= N; i++) {
+            for (int j = 1; j <= N; j++) {
+                arr[i][j] |= reverseArr[i][j];
+            }
+        }
+
+        int answer = 0;
+        outer:
+        for (int i = 1; i <= N; i++) {
+            for (int j = 1; j <= N; j++) {
+                if (i == j) {
+                    continue;
+                }
+                if (!arr[i][j]) {
+                    continue outer;
+                }
+            }
+            answer++;
+        }
+
+        bw.write(answer + "\n");
+        bw.flush();
+        bw.close();
+        br.close();
+    }
+}
