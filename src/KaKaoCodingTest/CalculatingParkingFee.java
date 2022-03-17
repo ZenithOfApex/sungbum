50651d46edbe038d9431eb9df97bf73b32089cf6
KangSungBum <peter9706@naver.com>
2022-03-17 17:29:22 +0900

    Problem Solved

--- src/KaKaoCodingTest/CalculatingParkingFee.java
+++ src/KaKaoCodingTest/CalculatingParkingFee.java
@@ -6,10 +6,15 @@ public class CalculatingParkingFee {
 
     static HashMap<Integer,CarInfo> carHashMap;
     static HashMap<Integer, Integer> totalFee;
+    static HashMap<Integer, Integer> totalTime;
 
     public static void main(String[] args) throws Exception {
-        int[] fees = {1, 461, 1, 10};
-        String[] records = {"00:00 1234 IN"};
+//        int[] fees = {1, 461, 1, 10};
+//        String[] records = {"00:00 1234 IN"};
+
+        int[] fees = {180, 5000, 10, 600};
+        String[] records = {"05:34 5961 IN", "06:00 0000 IN", "06:34 0000 OUT", "07:59 5961 OUT", "07:59 0148 IN", "18:59 0000 IN", "19:09 0148 OUT", "22:59 5961 IN", "23:00 5961 OUT"};
+
         int[] ans = solution(fees, records);
         for (int k : ans) {
             System.out.println(k);
@@ -20,11 +25,13 @@ public class CalculatingParkingFee {
     private static int[] solution(int[] fee, String[] records) {
         carHashMap = new HashMap<>();
         totalFee = new HashMap<>();
+        totalTime = new HashMap<>();
 
         for (String record : records) {//레코드마다 처리
             String[] recordArr = record.split(" ");
             int time = intoMinute(recordArr[0]);
             int carNum = Integer.parseInt(recordArr[1]);
+
             boolean recordStatus;
             if (recordArr[2].equals("IN")) {
                 recordStatus = true;
@@ -33,13 +40,20 @@ public class CalculatingParkingFee {
             //등록되어 있지 않은 경우 새로 등록
             if (!carHashMap.keySet().contains(carNum)) {
                 carHashMap.put(carNum, new CarInfo(time, recordStatus));
-            } else {//차가 이미 hashMap에 있는 경우에는 계산해야지
-                if (carHashMap.get(carNum).status) {//입차되어 있는 경우
-                    int inputTime = carHashMap.get(carNum).time;
-                    int calFee = calculateFee(inputTime, time, fee);
-                    totalFee.put(carNum, totalFee.get(carNum) + calFee);
+                totalTime.put(carNum, time);
+                totalFee.put(carNum, 0);
+                System.out.println(record);
+            } else {//차가 이미 hashMap에 있는 경우에는 시간만 계산하면 된다
+                if (carHashMap.get(carNum).status && !recordStatus) {//입차되어 있고 받은 기록이 out인 경우
+                    int enterTime = carHashMap.get(carNum).time;
+                    int durationTime = time - enterTime;
+                    totalTime.put(carNum, totalTime.get(carNum) + durationTime);
+                    carHashMap.get(carNum).status = false;
+//                    totalFee.put(carNum, totalFee.get(carNum) + calFee);
+                    System.out.println(record);
                 } else {//출차된 경우 새로운 입차 기록
                     carHashMap.put(carNum, new CarInfo(time, true));
+                    System.out.println(record);
                 }
             }
         }
@@ -47,13 +61,19 @@ public class CalculatingParkingFee {
         //모든 기록이 끝났는데 입차상태로 남아있는 애들은 23:59로 계산해서 반환해야된다
         for (Integer carNum : carHashMap.keySet()) {
             if (carHashMap.get(carNum).status) {//입차상태로 남아있는경우 계산
-                int calFee = calculateFee(carHashMap.get(carNum).time, intoMinute("23:59"), fee);
+                int durationTime = intoMinute("23:59") - carHashMap.get(carNum).time;
+                totalTime.put(carNum, totalTime.get(carNum) + durationTime);
+                int calFee = calculateFee(carHashMap.get(carNum).time,fee);
                 if (totalFee.get(carNum) == null) {
                     totalFee.put(carNum, calFee);
                 }else totalFee.put(carNum, totalFee.get(carNum) + calFee);
             } else continue;
         }
 
+        //최종계산
+        for (Integer value : totalTime.values()) {
+            System.out.println("value = " + value);
+        }
         //hashMap을 int형 배열로 반환
         int[] rtnNum = new int[carHashMap.size()];
         int[] rtnFee = new int[carHashMap.size()];
@@ -78,14 +98,12 @@ public class CalculatingParkingFee {
         return (60 * (Integer.parseInt(trimmedTime[0]))) + Integer.parseInt(trimmedTime[1]);
     }
 
-    //error catch하자
-    private static int calculateFee(int inTime, int outTime, int[] fee) {
+    private static int calculateFee(int durationTime, int[] fee) {
         int basicTime = fee[0];
         int basicFee = fee[1];
         int unitTime = fee[2];
         int unitFee = fee[3];
 
-        int durationTime = outTime - inTime;
         //주차 시간이 기본 시간 범위 내에 있을 경우
         if (durationTime <= basicTime) {
             return basicFee;
