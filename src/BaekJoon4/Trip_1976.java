47f2d579f76cd61111cc11c27f7e0a62082167f9
KangSungBum <peter9706@naver.com>
2022-04-23 01:34:04 +0900

    Problem Solved

--- src/BaekJoon4/Trip_1976.java
+++ src/BaekJoon4/Trip_1976.java
@@ -1,12 +1,13 @@
 package BaekJoon4;
 
 import java.io.*;
+import java.util.HashSet;
 import java.util.StringTokenizer;
 
 public class Trip_1976 {
     static int N, M;
     static String answer= "";
-    static int[] city;
+    static int[] city, plan;
 
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
@@ -16,17 +17,39 @@ public class Trip_1976 {
         N = Integer.parseInt(br.readLine());
         M = Integer.parseInt(br.readLine());//도시의 수
 
-        city = new int[N+1];
+        city = new int[N + 1];
+        plan = new int[M];
         for (int i = 1; i <= N; i++) {
             city[i] = i;
         }
 
+        //여기서 도시 연결 정보 처리
         for (int i = 0; i < N; i++) {
             st = new StringTokenizer(br.readLine(), " ");
-            int from = Integer.parseInt(st.nextToken());
-            int to = Integer.parseInt(st.nextToken());
-            int connected = Integer.parseInt(st.nextToken());
+            for (int j = 0; j < N; j++) {
+                if (Integer.parseInt(st.nextToken()) == 1) {
+                    if (!isSameGroup(i + 1, j + 1)) {
+                        union(i + 1, j + 1);
+                    }else continue;
+                }
+            }
         }
+
+        //마지막 정보 받고 처리 끝
+        st = new StringTokenizer(br.readLine(), " ");
+        for (int i = 0; i < M; i++) {
+            plan[i] = Integer.parseInt(st.nextToken());
+        }
+
+        HashSet<Integer> hs = new HashSet<>();
+        for (int i = 0; i < M; i++) {
+            int found = find(plan[i]);
+            hs.add(found);
+        }
+
+        if (hs.size() == 1) {
+            answer = "YES";
+        } else answer = "NO";
 
         bw.write(answer + "\n");
         bw.flush();
