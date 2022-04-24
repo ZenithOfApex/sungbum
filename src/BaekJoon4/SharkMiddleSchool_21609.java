cd6d157200dd32632dc94b5a1b03e7db42f0857b
KangSungBum <peter9706@naver.com>
2022-04-24 15:54:53 +0900

    Problem Solved

--- src/BaekJoon4/SharkMiddleSchool_21609.java
+++ src/BaekJoon4/SharkMiddleSchool_21609.java
@@ -7,10 +7,13 @@ import java.util.Queue;
 import java.util.StringTokenizer;
 
 public class SharkMiddleSchool_21609 {
+    static int INF = Integer.MAX_VALUE;
     static int N, M;
     static int answer;
+    static int maximumSize = Integer.MIN_VALUE;
     static int[][] map;
     static int[][] blockCnt;
+    static ArrayList<Block> bigBlock = new ArrayList<>();
     static int[] dx = {-1, 0, 1, 0};
     static int[] dy = {0, 1, 0, -1};
 
@@ -34,6 +37,16 @@ public class SharkMiddleSchool_21609 {
             }
         }
 
+        while (bigBlock.size() != 1) {
+            findBiggestBlock();
+            deleteBiggestBlock(bigBlock);
+            getScore(bigBlock.size());
+            applyGravity();
+            rotateClockwise();
+            applyGravity();
+            print();
+        }
+
         bw.write(answer + "\n");
         bw.flush();
         bw.close();
@@ -49,12 +62,12 @@ public class SharkMiddleSchool_21609 {
                 //시작 블럭 개수 count
                 int cnt = 1;
                 //blockList에 포함되는 block들
-                ArrayList<Integer> blockList = new ArrayList<>();
-                blockList.add(fixedNormalBlock);
+                ArrayList<Block> blockList = new ArrayList<>();
+                blockList.add(new Block(i,j,fixedNormalBlock));
 
                 //bfs에서 사용할 큐
                 Queue<Block> blockQueue = new LinkedList<>();
-                blockQueue.add(new Block(i, j));
+                blockQueue.add(new Block(i, j, map[i][j]));
 
                 while (!blockQueue.isEmpty()) {
                     Block block = blockQueue.poll();
@@ -74,10 +87,15 @@ public class SharkMiddleSchool_21609 {
                             continue;
                         }
 
-                        blockQueue.add(new Block(nx, ny));
-                        blockList.add(map[nx][ny]);
+                        blockQueue.add(new Block(nx, ny,map[nx][ny]));
+                        blockList.add(new Block(nx, ny, map[nx][ny]));
                         cnt++;
                     }
+                }
+                //블럭의 크기 저장
+                maximumSize = Math.max(maximumSize, blockList.size());
+                if (blockList.size() == maximumSize) {
+                    bigBlock = new ArrayList<>(blockList);
                 }
                 if (checkBlockCondition(blockList)) {
                     blockCnt[i][j] = blockList.size();
@@ -86,10 +104,10 @@ public class SharkMiddleSchool_21609 {
         }
     }
 
-    private static boolean checkBlockCondition(ArrayList<Integer> blockList) {
+    private static boolean checkBlockCondition(ArrayList<Block> blockList) {
         int cnt = 0;
-        for (Integer block : blockList) {
-            if (block != 0) {
+        for (Block block : blockList) {
+            if (block.value != 0) {
                 cnt++;
             }
         }
@@ -99,26 +117,19 @@ public class SharkMiddleSchool_21609 {
     }
 
     //가장 큰 블럭 삭제
-    private static void deleteBiggestBlock(){
-        int bigNum = Integer.MIN_VALUE;
-        for (int i = 0; i < N; i++) {
-            for (int j = 0; j < N; j++) {
-                bigNum = Math.max(bigNum, blockCnt[i][j]);
-            }
+    private static void deleteBiggestBlock(ArrayList<Block> blockList){
+        for (Block block : blockList) {
+            map[block.x][block.y] = INF;
         }
-
-        //bigNum으로 bfs 돌려서 삭제
     }
 
     //2차원 배열 반시계로 90도 회전
-    private static int[][] rotateClockwise(int arr[][]){
-        int n = arr.length;
-        int m = arr[0].length;
-        int[][] rotate = new int[m][n];
+    private static int[][] rotateClockwise(){
+        int[][] rotate = new int[N][N];
 
         for(int i=0;i<rotate.length;i++){
             for (int j = 0; j < rotate[i].length; j++) {
-                rotate[i][j] = arr[n - 1 - j][i];
+                rotate[i][j] = map[N - 1 - j][i];
             }
         }
 
@@ -127,25 +138,47 @@ public class SharkMiddleSchool_21609 {
 
     //이게 어려울듯
     private static void applyGravity(){
-
+        for (int i = N - 1; i >= 0; i--) {
+            for (int j = 0; j < N; j++) {
+                if (map[i][j] >= 0) {
+                    //아래칸이 범위안에 있고 비어있는 칸일때만 하나 이동이 아니라 쭉
+                    while (!inRange(i + 1, j) && map[i + 1][j] == INF) {
+                        map[i+1][j] = map[i][j];
+                        map[i][j] = INF;
+                    }
+                }
+            }
+        }
     }
 
     //블럭의 수 필요
-    private static int getScore(int blockCnt){
-        return (int)Math.pow(blockCnt, 2);
+    private static void getScore(int blockCnt){
+        answer+= (int)Math.pow(blockCnt, 2);
     }
 
     private static boolean inRange(int x, int y) {
         return x >= 0 && x < N && y >= 0 && y < N;
     }
 
+    private static void print() {
+        for (int[] ints : map) {
+            for (int anInt : ints) {
+                System.out.print(anInt);
+            }
+            System.out.println();
+        }
+        System.out.println();
+    }
+
     private static class Block{
         int x;
         int y;
+        int value;
 
-        public Block(int x, int y) {
+        public Block(int x, int y, int value) {
             this.x = x;
             this.y = y;
+            this.value = value;
         }
     }
 }
